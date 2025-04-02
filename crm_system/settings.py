fb1a652a220cac91eb1ccf66f9d8760d7d2ddc8b
Samarthwealth <trading@samarthedufin.com>
2025-04-02 22:14:17 +0530

    Update settings.py

--- crm_system/settings.py
+++ crm_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1lzz(b&eai-hv@44n48n6lz#ofc4%-=unm0eg7#$hlce+0ggay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
